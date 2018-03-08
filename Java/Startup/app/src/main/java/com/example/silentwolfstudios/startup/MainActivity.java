5ed4a6554609bc2596cbca6ca1506a95538ec008
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-03-08 13:12:22 +0800

    created new fos FileOutputStream set using openFileOutput (TEXTFILENAME,Context.MODE_PRIVATE); Then pass in your text in bytes to be written using fos.write(etDesText.getBytes()); Then Close your FileOutStream using fos.close()

--- Java/Startup/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
+++ Java/Startup/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.silentwolfstudios.startup;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity { //ctrl + p is used to show parameters of a method
+
+    public static final String DEBUGTAG ="NWY";  // final means can only be assigned once
+    public static final String TEXTFILE = "notesquirrel.txt"; //Finame
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
         saveBtn.setOnClickListener( new View.OnClickListener(){ //creating OnClickListener // new is an anymous class
             @Override
             public void onClick(View v) { // Must override View //Whatever's written in this method will be called
-                saveBtn.setText("ok");
+               EditText etDes = (EditText) findViewById(R.id.etDes); //Cast to EditText
+
+                String etDesText = etDes.getText().toString();
+
+                try {
+                   FileOutputStream fos= openFileOutput(TEXTFILE, Context.MODE_PRIVATE) ; //save text to buffer file as Private , cant be accessed by user
+                    fos.write(etDesText.getBytes()); //pass number of bytes of input from etDesText
+                    fos.close();
+                } catch (Exception ex){
+                    ex.printStackTrace(); //tells you where exception occured
+                    Log.d(DEBUGTAG, "Unable to save file");
+                }
+
+                Log.d(DEBUGTAG, "SaveButtonClicked: "+ etDesText); //Debug Logs to LogCat only// Must enable Debuggable on LogCat to see the Log
+
             }
         }) ;
 
