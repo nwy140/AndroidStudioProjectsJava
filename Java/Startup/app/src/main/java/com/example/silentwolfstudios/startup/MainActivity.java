7e7f2714fc76c3f6a41aa20df2c09fbbcac3951b
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-03-08 10:39:22 +0800

    Created new Method addSaveButtonListener which gets SaveBtn, SetOnClickListener and passes new View.OnClicklistener method which calls another method to override onClick(View v) , whatever is in onClick method will be called each time Button is Pressed.

--- Java/Startup/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
+++ Java/Startup/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.silentwolfstudios.startup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //setContentView method sets Activity currently being displayed
 
-        String t;
+        addSaveButtonListener();
+    }
+
+    private  void addSaveButtonListener(){
+        final Button saveBtn = (Button)findViewById(R.id.save); //Access Item With ID Save from Activity.Main
+                            // Cast to Button
+        saveBtn.setOnClickListener( new View.OnClickListener(){ //creating OnClickListener // new is an anymous class
+            @Override
+            public void onClick(View v) { // Must override View //Whatever's written in this method will be called
+                saveBtn.setText("ok");
+            }
+        }) ;
 
 
     }
