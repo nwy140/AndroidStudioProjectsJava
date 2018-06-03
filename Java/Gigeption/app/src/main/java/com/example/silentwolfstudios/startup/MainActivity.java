6149ed8181783130e6d20b1ff519ecc0b1a3cde5
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-06-03 12:41:54 +0800

    In MainActivity, when btnFunFact is clicked, change background color to a random color and funfactdes to a random funfact

--- Java/Gigeption/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
+++ Java/Gigeption/app/src/main/java/com/example/silentwolfstudios/startup/MainActivity.java
@@ -19,6 +19,8 @@ import com.google.android.gms.ads.InterstitialAd;
 
 import com.google.android.gms.ads.AdListener;
 
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity { //ctrl + p is used to show parameters of a method
 
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity { //ctrl + p is used to show
     Button btnFunFact ;
     ConstraintLayout mConstraint;
 
+    Random randomGenerator;
+    int randomNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,29 +40,52 @@ public class MainActivity extends AppCompatActivity { //ctrl + p is used to show
         MobileAds.initialize(this, "ca-app-pub-3043579075978700~9054078114");
 
         loadInterstitialAd();
-        getIds();
+        getIdsandFacts();
         addbtnFunFactListener();
     }
 
-    private void getIds(){
+    private void getIdsandFacts(){
         tvDidYouKnow = (TextView)findViewById(R.id.tvDidYouKnow);
         tvFactDes = (TextView)findViewById(R.id.tvFactDes);
         btnFunFact = (Button)findViewById(R.id.btnFunFact);
         mConstraint = (ConstraintLayout)findViewById(R.id.mConstraint);
-    }
 
+
+    }
 
 private void addbtnFunFactListener(){
       //  btnFunFact = (Button)findViewById(R.id.btnFunFact);
 
+    randomGenerator = new Random();
+    final String[] facts= {
+
+            "Ostrict can fly",
+            "Ostrict cannot jump",
+            "Ostrict cannot fly",
+            "Ostrict eats humans",
+            "Ostrict can live for 1000 years"
+    };
+    final String[] colors= {
+            "#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6",
+            "#E6B333", "#3366E6", "#999966", "#99FF99", "#B34D4D",
+            "#80B300", "#809900", "#E6B3B3", "#6680B3", "#66991A",
+            "#FF99E6", "#CCFF1A", "#FF1A66", "#E6331A", "#33FFCC",
+            "#66994D", "#B366CC", "#4D8000", "#B33300", "#CC80CC",
+            "#66664D", "#991AFF", "#E666FF", "#4DB3FF", "#1AB399",
+            "#E666B3", "#33991A", "#CC9999", "#B3B31A", "#00E680",
+            "#4D8066", "#809980", "#E6FF80", "#1AFF33", "#999933",
+            "#FF3380", "#CCCC00", "#66E64D", "#4D80CC", "#9900B3",
+            "#E64D66", "#4DB380", "#FF4D4D", "#99E6E6", "#6666FF"
+    };
         btnFunFact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 //tvFactDes = (TextView)findViewById(R.id.tvFactDes);
-                Toast.makeText(getApplicationContext(), "Button Clicked " + tvFactDes.getText().toString(), Toast.LENGTH_SHORT).show();
-                tvFactDes.setText("Ostrict can fly");
-                mConstraint.setBackgroundColor(Color.BLUE);
+            //    Toast.makeText(getApplicationContext(), "Button Clicked " + tvFactDes.getText().toString(), Toast.LENGTH_SHORT).show();
+                randomNumbers = randomGenerator.nextInt(facts.length);
+               tvFactDes.setText(facts[randomNumbers]);
+               randomNumbers = randomGenerator.nextInt(colors.length);
+                mConstraint.setBackgroundColor(Color.parseColor(colors[randomNumbers]));
             }
         });
 }
