f85f82926675858ff3b9fa48bd6dea77588171af
Nwy <nwy140@gmail.com>
2019-03-15 11:16:24 +0800

    Added krabsrobotapp

---
+++ Java/MrKrabsRobotDance/app/src/main/java/com/example/silentwolfstudios/startup/Splash.java
@@ -0,0 +1,38 @@
+package com.example.silentwolfstudios.startup;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_splash);
+
+        MediaPlayer splashsound = MediaPlayer.create(Splash.this,R.raw.bibububabububi);
+        splashsound.start();
+
+
+        //spash screen workaround admob ads policy
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+
+}
